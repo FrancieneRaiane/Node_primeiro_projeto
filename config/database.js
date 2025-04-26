c16ef1ce014ed9f3ce644f064b38b2e008303462
FrancieneRaiane <franciene.sjdr@gmail.com>
2025-04-25 21:17:24 -0300

    primeiro commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql2/promise';
+import dotnev from 'dotenv'
+dotnev.config();
+
+const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+try {
+    await connection.execute('SELECT 1');
+    console.log('Conexão estabelecida!');
+   } catch (error) {
+    console.error('Erro ao conectar:', error);
+   }
+
+   export default connection;
+   
