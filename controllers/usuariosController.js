cf8157fc91076d3273031a224feabbfb698d0e7c
FrancieneRaiane <franciene.sjdr@gmail.com>
2025-04-30 21:34:11 -0300

    ATUALIZAÇAO

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -39,7 +39,86 @@ const usuariosController ={
             })
 
         }
+    },
+    async buscarPorId(req, res){
+        try{
+            const { id } = req.params;
+            const [usuario] = await connection.execute(
+                'SELECT * FROM usuarios WHERE id = ?',
+                [id]
+            )
+            if (usuario.length === 0){
+                return res.status (404).json({
+                    success:false,
+                    message: "usuario nao encontrado"
+                })
+            }
+
+            return res.status(201).json({
+                success:true,
+                data: usuario [0]
+            })
+        }catch (error){
+            console.error("Erro ao buscar usuario", error)
+            return res.status(500).json({
+                success: false,
+                message: "erro ao buscar usuario",
+                error: error.message
+
+            })
+
+        }
+    },
+    async alterarDados (req, res){
+        try{
+            const {id} = req.params;
+            const {nome, email, idade} = req.body;
+
+            const camposAtualizar = [];
+            const valores = []
+
+            if (nome){
+                camposAtualizar.push('nome =?'),
+                valores.push(nome)
+            }
+            if (email){
+                camposAtualizar.push('email =?'),
+                valores.push(email)
+            }
+            if (idade){
+                camposAtualizar.push('idade =?'),
+                valores.push(idade)
+            }
+
+            valores.push (id)
+            const [result] = await  connection.execute(
+                `UPDATE usuarios SET ${camposAtualizar.join(', ')} WHERE id = ?`,
+                valores
+            );
+
+            if (result.affectedRows === 0){
+                return res.status(404).json({
+                    success: false,
+                    message: "usurios nao encontrado"
+                })
+            }
+
+            return res.status(200).json({
+                success: true,
+                message: `Usuario ${id}  atualizado com sucesso`,
+                registrosAfetados: result.affectedRows
+            })
+
+        }catch (error){
+            console.error("Erro ao alterar usuario", error)
+            return res.status(500).json({
+                success: false,
+                message: "erro ao alterar usuario",
+                error: error.message
+            })
+        }
     }
+
 }
 
 export default usuariosController;
--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -5,6 +5,8 @@ import usuariosController from '../controllers/usuariosController.js';
 
 const router = express.Router();
 router.post('/users', usuariosController.criar); //http://localhost/api/users
-router.get('/users', usuariosController.listarUsuarios)
+router.get('/users', usuariosController.listarUsuarios);
+router.get('/users/:id', usuariosController.buscarPorId);
+router.put('/users/:id', usuariosController.alterarDados);
 
 export default router;
